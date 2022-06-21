6f7c1c59872eacf7cea8e5a71c0101af16122dd5
raysas <91915465+raysas@users.noreply.github.com>
2022-06-21 09:54:46 +0300

    Update variables-and-some-arithmetic.py

--- Python Village/variables-and-some-arithmetic.py
+++ Python Village/variables-and-some-arithmetic.py
@@ -3,6 +3,7 @@
 
 '''Given: Two positive integers a and b, each less than 1000.
 Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.'''
+
 a=int(input('Enter first side of the right triangle:'))
 b=int(input('Enter second side of the right triangle:'))
 hyp_sq=a**2+b**2
